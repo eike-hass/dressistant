93e5086c21b7bea75e53352831a55daecffab37a
Niclas Holtappels <niclas@holtappels.in>
2016-07-09 00:12:49 +0200

    Product Content befüllt

--- src/product.js
+++ src/product.js
@@ -1,11 +1,11 @@
 export default [{
   id: '1',
-  title: 'Test Title 1',
-  description: 'Test Description',
+  title: 'T-SHIRT MIT V-AUSSCHNITT',
+  description: '100% Baumwolle',
   images: [
-    'http://lorempixel.com/400/200/people/1',
-    'http://lorempixel.com/400/200/people/2',
-    'http://lorempixel.com/400/200/people/3',
+    '/shirt_a_1.jpg',
+    '/shirt_a_2.jpg',
+    '/shirt_a_3.jpg',
   ],
   sizes: [
     's',
@@ -13,32 +13,35 @@ export default [{
     'l',
   ],
   colors: [
-    'red',
-    'blue',
-    'green',
+    '#d5dee3',
+    '#d96f3d',
+    '#bb7d4c',
   ],
   price: {
-    regular: 10.00,
-    discounted: null,
+    regular: 12.99,
+    discounted: 9.99,
   },
   relatedProducts: [
-    '2',
+    '',
   ],
   likeStatus: null,
 },
 {
   id: '2',
-  title: 'Test Title 2',
-  description: 'Test Description',
+  title: '7/8 KHAKI IM SLIM-LOOK',
+  description: '51% Baumwolle, 41% Polyamid, 8% Elastan',
   images: [
-    'http://lorempixel.com/400/200/people/1',
-    'http://lorempixel.com/400/200/people/2',
-    'http://lorempixel.com/400/200/people/3',
+    '/pants_a_1.jpg',
+    '/pants_a_2.jpg',
+    '/pants_a_3.jpg',
   ],
   sizes: [
-    's',
-    'm',
-    'l',
+    '36',
+    '38',
+    '40',
+    '42',
+    '44',
+    '46',
   ],
   colors: [
     'red',
@@ -46,11 +49,86 @@ export default [{
     'green',
   ],
   price: {
-    regular: 10.00,
-    discounted: null,
+    regular: 49.99,
+    discounted: 19.99,
+  },
+  relatedProducts: [
+      '3',
+      '4',
+      '5',
+  ],
+  likeStatus: null,
+},
+{
+  id: '3',
+  title: 'MONKSTRAP AUS LEDER',
+  description: 'SCHNITT: 100% Kuhleder\r\nFUTTER: 70% Baumwolle, 30% Polyester\r\nSOHLE: 100% Thermoplastischer Kautschuk\r\nINNENSOHLE: 100% Mestizo',
+  images: [
+    '/shoe_a_1.jpg',
+    '/shoe_a_2.jpg',
+    '/shoe_a_3.jpg',
+  ],
+  sizes: [
+    '41',
+    '42',
+    '43',
+    '44',
+    '45',
+  ],
+  colors: [
+    '#1e1c21',
+  ],
+  price: {
+    regular: 49.99,
+    discounted: 29.99,
+  },
+  relatedProducts: [],
+  likeStatus: null,
+},
+{
+  id: '4',
+  title: 'STROHHUT',
+  description: '100% Zellulosepaste',
+  images: [
+    '/hat_a_1.jpg',
+    '/hat_a_2.jpg',
+    '/hat_a_3.jpg',
+  ],
+  sizes: [
+    's',
+    'l',
+  ],
+  colors: [
+    '#0f1417',
+    '#0c0c0a',
+  ],
+  price: {
+    regular: 15.99,
+    discounted: 9.99,
+  },
+  relatedProducts: [],
+  likeStatus: null,
+},
+{
+  id: '5',
+  title: 'LEICHTE BOWLINGTASCHE',
+  description: '100% Polyester',
+  images: [
+    '/bag_a_1.jpg',
+    '/bag_a_2.jpg',
+    '/bag_a_3.jpg',
+  ],
+  sizes: [
+    'm',
+  ],
+  colors: [
+    '#3b4777',
+  ],
+  price: {
+    regular: 49.99,
+    discounted: 24.99,
   },
   relatedProducts: [],
   likeStatus: null,
 },
 ];
-
